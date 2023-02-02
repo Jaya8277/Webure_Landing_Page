d8e1d8318caec399098875c3614d6164ffbe42a9
Jaya Vishwakarma <jayavish423@gmail.com>
2023-02-03 00:17:39 +0530

    navbar done

---
+++ app/src/Components/Home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="m-auto border-2 border-black max-w-[1440px] min-h-[780px] bg-[#FFFFFF]">Home</div>
+  )
+}
+
+export default Home
